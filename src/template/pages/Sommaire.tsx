1944f5746ebd07d9a67be623376a3022b29d06f9
alangainie <alan.gainie@epitech.eu>
2026-01-01 22:55:28 +0100

    update Somaire. Is set autmaticalaly by current datas Menue

--- src/template/pages/Sommaire.tsx
+++ src/template/pages/Sommaire.tsx
@@ -1,34 +1,50 @@
+import {MultipliedMenues} from '../../../datas/Menue.tsx'
+import React from 'react';
+
+function convert_composant_to_name_string(component: any): string {
+    // Si c'est un élément JSX, on récupère le type (la fonction)
+    const comp = component?.type || component;
+
+    // Récupère displayName si défini, sinon name, sinon "AnonymousComponent"
+    if (comp?.displayName) return comp.displayName;
+    if (comp?.name) return comp.name;
+
+    // Si c'est un élément React simple (string, Fragment, etc.)
+    if (typeof comp === 'string') return comp;
+
+    return "AnonymousComponent";
+}
+
 function Sommaire() {
     return (
         <>
             <h1><u>Sommaire</u></h1>
-            <p>Dans ce document vous retrouverer diférentes partie explicative sur le candidat :
-                <br /> <br />
-                Home page : <br />
-                - Introduction <br />
-                - Sommaire <br />
-                A propos de moi/CV : <br />
-                - A propos de moi <br />
-                - Curiculum Vitae <br />
-                Mes compétences : <br />
-                - Javascript <br />
-                - React <br />
-                - Node.js <br />
-                Projets : <br />
-                - Projet1 <br />
-                - Projet2 <br />
-                Mes études : <br />
-                - Lycée <br />
-                - Epitech <br />
-                - ESMA <br />
-                - ESUP <br />
-                Contact : <br />
-                - Linkedin <br />
-                - Gmail <br />
-                - Téléphone <br />
+            <p>
+                Dans ce document vous retrouverez différentes parties explicatives sur le candidat :
+                <br /><br />
+                {MultipliedMenues.map((menu, menuIndex) => (
+                    <span key={menuIndex}>
+                        {menu.names[0] && (
+                            <>
+                                {menuIndex === 0
+                                    ? "Home page"
+                                    : menuIndex === 1
+                                    ? "À propos de moi/CV"
+                                    : menu.names.join(' / ')} 
+                                : <br />
+                            </>
+                        )}
+                        {menu.sections.map((section, index) => (
+                            <React.Fragment key={index}>
+                                - {convert_composant_to_name_string(section)} <br />
+                            </React.Fragment>
+                        ))}
+                        <br />
+                    </span>
+                ))}
             </p>
         </>
-    )
+    );
 }
 
 export default Sommaire
