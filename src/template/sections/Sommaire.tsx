d9ade4575ee4ee36cfd9072c421bea2b51d27597
alangainie <alan.gainie@epitech.eu>
2026-04-27 05:44:12 +0200

    add build

--- src/template/sections/Sommaire.tsx
+++ src/template/sections/Sommaire.tsx
@@ -1,50 +1,40 @@
-import {MultipliedMenues} from '../../../datas/Menue.tsx'
+import MenueData from '../../../datas/Menue.json'
 import React from 'react';
 
-function convert_composant_to_name_string(component: any): string {
-    // Si c'est un élément JSX, on récupère le type (la fonction)
-    const comp = component?.type || component;
+// function convert_composant_to_name_string(component: any): string {
+//     // Si c'est un élément JSX, on récupère le type (la fonction)
+//     const comp = component?.type || component;
 
-    // Récupère displayName si défini, sinon name, sinon "AnonymousComponent"
-    if (comp?.displayName) return comp.displayName;
-    if (comp?.name) return comp.name;
+//     // Récupère displayName si défini, sinon name, sinon "AnonymousComponent"
+//     if (comp?.displayName) return comp.displayName;
+//     if (comp?.name) return comp.name;
 
-    // Si c'est un élément React simple (string, Fragment, etc.)
-    if (typeof comp === 'string') return comp;
+//     // Si c'est un élément React simple (string, Fragment, etc.)
+//     if (typeof comp === 'string') return comp;
 
-    return "AnonymousComponent";
-}
+//     return "AnonymousComponent";
+// }
 
 function Sommaire() {
     return (
         <>
-            <h1><u>Sommaire</u></h1>
+            <h1>
+                <u>Sommaire</u>
+            </h1>
+
             <p>
                 Dans ce document vous retrouverez différentes parties explicatives sur le candidat :
                 <br /><br />
-                {MultipliedMenues.map((menu, menuIndex) => (
-                    <span key={menuIndex}>
-                        {menu.names[0] && (
-                            <>
-                                {menuIndex === 0
-                                    ? "Home page"
-                                    : menuIndex === 1
-                                    ? "À propos de moi/CV"
-                                    : menu.names.join(' / ')} 
-                                : <br />
-                            </>
-                        )}
-                        {menu.sections.map((section, index) => (
-                            <React.Fragment key={index}>
-                                - {convert_composant_to_name_string(section)} <br />
-                            </React.Fragment>
-                        ))}
+
+                {MenueData.sections.map((section) => (
+                    <React.Fragment key={section.id}>
+                        - {section.label}
                         <br />
-                    </span>
+                    </React.Fragment>
                 ))}
             </p>
         </>
     );
 }
 
-export default Sommaire
+export default Sommaire;
